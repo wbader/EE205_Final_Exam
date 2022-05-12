d068283831a734c661d62212b1ef3b39d016338d
Waylon Bader <wbader@hawaii.edu>
2022-05-12 10:07:22 -0400

    update check for github

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Just making sure!" << std::endl;
+    std::cout << "Can I update github?" << std::endl;
     return 0;
 }
