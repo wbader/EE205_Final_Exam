3c50730cac3ee8b16dc584da3ce7dd7bd336b887
Waylon Bader <wbader@hawaii.edu>
2022-05-13 16:14:13 -0400

    created Square.cpp/.h

---
+++ Square.h
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  EE205_Final_Exam - EE 205 - Spr 2022
+///
+/// @file Square.h
+/// @version 1.0
+///
+/// @author Waylon Bader <wbader@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "Rectangle.h"
+
+class Square : public Rectangle {
+
+};
+
